bcde9e2b1a7031892eb72e792b15e516ead3bd26
Ariete <alefiap@hotmail.com>
2017-02-10 21:59:03 -0200

    TESTE DORIA!

---
+++ teste.java
@@ -0,0 +1,17 @@
+public class teste{
+
+	public static void main(String[] args){
+
+		System.out.println("Alô Mundo!");
+		System.out.println("Alô Mundo!");
+		
+		System.out.println("Alô Mundo!");
+		System.out.println("Alô Mundo!");
+
+		System.out.println("Alô Mundo!");
+		System.out.println("Alô Mundo!");
+
+
+	}
+
+}
